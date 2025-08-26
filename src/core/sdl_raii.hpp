f7663a101b3721dab5164a30ee4ab911a5d95a45
blomq <blomqvistkevin@hotmail.com>
2025-08-26 16:09:49 +0200

    Add RAII wrappers for SDL window and Metal view creation
    Introduced `CreateWindow` and `CreateMetalView` utility functions in the `sdl_raii` namespace to wrap SDL resource management with RAII, improving code safety and readability.

--- src/core/sdl_raii.hpp
+++ src/core/sdl_raii.hpp
@@ -6,14 +6,28 @@
 #define PSYGINE_SDL_RAII_HPP
 
 #include <memory>
-#include <SDL3/SDL_metal.h>
 
+#include <SDL3/SDL_metal.h>
 #include <SDL3/SDL_video.h>
 
 namespace psygine::core
 {
     using SdlWindowPtr = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
     using SdlMetalViewPtr = std::unique_ptr<SDL_MetalView, decltype(&SDL_Metal_DestroyView)>;
+
+    namespace sdl_raii {
+        template<typename... Args>
+        [[nodiscard]] SdlWindowPtr CreateWindow(Args&&... args)
+        {
+            return SdlWindowPtr(SDL_CreateWindow(std::forward<Args>(args)...), &SDL_DestroyWindow);
+        }
+
+        template<typename... Args>
+        [[nodiscard]] SdlMetalViewPtr CreateMetalView(Args&&... args)
+        {
+            return SdlMetalViewPtr(SDL_Metal_CreateView(std::forward<Args>(args)...), &SDL_Metal_DestroyView);
+        }
+    }
 }
 
 #endif //PSYGINE_SDL_RAII_HPP
